1724124acf884aea57f79aa2bc392f400aa8df75
GiusTex <112352961+GiusTex@users.noreply.github.com>
2024-09-29 01:45:32 +0200

    Added Encode Diffusers Outpaint Prompt

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from .nodes import (PadImageForDiffusersOutpaint, LoadDiffusersOutpaintModels, DiffusersImageOutpaint)
+from .nodes import (PadImageForDiffusersOutpaint, LoadDiffusersOutpaintModels, EncodeDiffusersOutpaintPrompt, DiffusersImageOutpaint)
 
 NODE_CLASS_MAPPINGS = {
     "PadImageForDiffusersOutpaint": PadImageForDiffusersOutpaint,
     "LoadDiffusersOutpaintModels": LoadDiffusersOutpaintModels,
+    "EncodeDiffusersOutpaintPrompt": EncodeDiffusersOutpaintPrompt,
     "DiffusersImageOutpaint": DiffusersImageOutpaint
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PadImageForDiffusersOutpaint": "Pad Image For Diffusers Outpaint",
     "LoadDiffusersOutpaintModels": "Load Diffusers Outpaint Models",
+    "EncodeDiffusersOutpaintPrompt": "Encode Diffusers Outpaint Prompt",
     "DiffusersImageOutpaint": "Diffusers Image Outpaint"
 }
